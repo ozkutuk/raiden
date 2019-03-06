d432e36bd24cbfc7c01c3c859823bf4f8e912de0
Berk Özkütük <berkobrathestabber@gmail.com>
2019-03-06 17:11:02 +0300

    awful diffuse shading

--- main.cpp
+++ main.cpp
@@ -8,6 +8,7 @@
 
 #include "stb_image_write.h"
 
+#include <algorithm>
 #include <cstdint>
 #include <limits>
 #include <vector>
@@ -23,8 +24,13 @@ struct Color {
 };
 
 struct HitRecord {
-    const Sphere * sphere;
+    const Sphere *sphere;
     float t;
+};
+
+struct Light {
+    tmath::vec3f intensity;
+    tmath::vec3f position;
 };
 
 HitRecord scene_hit(const Ray &ray, const std::vector<Sphere> &spheres) {
@@ -37,7 +43,7 @@ HitRecord scene_hit(const Ray &ray, const std::vector<Sphere> &spheres) {
             closest = &sphere;
         }
     }
-    
+
     HitRecord hit;
     if (distance < 1000)
         hit.sphere = closest;
@@ -47,7 +53,8 @@ HitRecord scene_hit(const Ray &ray, const std::vector<Sphere> &spheres) {
     return hit;
 }
 
-tmath::vec3f color(const Ray &ray, const std::vector<Sphere> &spheres) {
+tmath::vec3f cast_ray(const Ray &ray, const std::vector<Sphere> &spheres,
+                      const std::vector<Light> &lights) {
 
     HitRecord hit = scene_hit(ray, spheres);
     if (!hit.sphere) {
@@ -59,16 +66,25 @@ tmath::vec3f color(const Ray &ray, const std::vector<Sphere> &spheres) {
         return (1.0f - k) * white + k * blue;
     }
 
-    return hit.sphere->material.diffuse;
+    tmath::vec3f diffuse_light_intensity(0.0f, 0.0f, 0.0f);
+    tmath::vec3f hit_point = ray.at(hit.t);
+    tmath::vec3f normal = tmath::normalize(hit_point - hit.sphere->center);
+    for (const auto &light : lights) {
+        tmath::vec3f light_dir = tmath::normalize(light.position - hit_point);
+        diffuse_light_intensity += light.intensity * std::max(0.0f, tmath::dot(light_dir, normal));
+    }
+
+    return hit.sphere->material.diffuse * diffuse_light_intensity;
 }
 
-void render(const std::vector<Sphere> &spheres) {
+void render(const std::vector<Sphere> &spheres, const std::vector<Light> &lights) {
     constexpr int image_width = 1920;
     constexpr int image_height = 1080;
 
     // NOTE: assume camera is at 0,0,0 for now
+    // TODO: create a proper camera
     tmath::vec3f origin(0.0f, 0.0f, 0.0f);
-    tmath::vec3f top_left(-8.0f, 4.5f, -1.0f);
+    tmath::vec3f top_left(-8.0f, 4.5f, -4.0f);
     tmath::vec3f horizontal(16.0f, 0.0f, 0.0f);
     tmath::vec3f vertical(0.0f, -9.0f, 0.0f);
     tmath::vec3f u = horizontal / image_width;
@@ -79,7 +95,7 @@ void render(const std::vector<Sphere> &spheres) {
     for (int y = 0; y < image_height; y++) {
         for (int x = 0; x < image_width; x++) {
             Ray r(origin, top_left + x * u + y * v);
-            tmath::vec3f value = color(r, spheres);
+            tmath::vec3f value = cast_ray(r, spheres, lights);
             Color c;
             c.r = static_cast<uint8_t>(value.x * 255.99f);
             c.g = static_cast<uint8_t>(value.y * 255.99f);
@@ -96,10 +112,16 @@ void render(const std::vector<Sphere> &spheres) {
 int main(void) {
     Material darkred(tmath::vec3f(0.7f, 0.0f, 0.3f));
     Material beige(tmath::vec3f(0.9f, 0.9f, 0.7f));
-    Sphere sphere(tmath::vec3f(-4.0f, 0.0f, -2.0f), 1.75f, darkred);
-    Sphere sphere2(tmath::vec3f(4.0f, 2.0f, -3.0f), 2.0f, beige);
+    Sphere sphere(tmath::vec3f(-4.0f, 0.0f, -7.0f), 1.75f, darkred);
+    Sphere sphere2(tmath::vec3f(4.0f, 2.0f, -9.0f), 2.0f, beige);
     std::vector<Sphere> spheres = {sphere, sphere2};
 
-    render(spheres);
+    Light light;
+    light.position = tmath::vec3f(-6.0f, 4.0f, -5.0f);
+    tmath::vec3f white = tmath::vec3f(0.9f, 0.9f, 0.9f);
+    light.intensity = white;
+    std::vector<Light> lights = {light};
+
+    render(spheres, lights);
     return EXIT_SUCCESS;
 }
